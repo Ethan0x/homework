7ecd4bdf9319d5315b968e6dccbd7d20044fc46b
Kook0x <jj.mac@live.cn>
2021-08-10 11:17:39 +0800

    Add 84.largest_rectangle_in_histogram

---
+++ week1/84.largest_rectangle_in_histogram/84.largest_rectangle_in_histogram.java
@@ -0,0 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Arrays.fill(right, n);
+
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                right[stack.peek()] = i;
+                stack.pop();
+            }
+            left[i] = (stack.isEmpty() ? -1 : stack.peek());
+            stack.push(i);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
+    }
+}

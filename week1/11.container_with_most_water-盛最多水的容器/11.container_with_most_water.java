b97cb446d890f696d38fd3a26fe7f56164e4a999
Kook <jj.mac@live.cn>
2021-08-28 21:44:57 +0800

    Fix

--- week1/11.container_with_most_water-盛最多水的容器/11.container_with_most_water.java
+++ week1/11.container_with_most_water-盛最多水的容器/11.container_with_most_water.java
@@ -2,8 +2,8 @@
 class Solution {
     public int maxArea(int[] a) {
         int max = 0;
-        for(int i =0; i< a.length-1； i++){
-            for (int j = i+1; j < a.length -1; j++) {
+        for (int i = 0; i < a.length - 1; i++){
+            for (int j = i+1; j < a.length; j++) {
                 int area = (j-i) * Math.min(a[i], a[j]);
                 max = Math.max(max, area);
             }

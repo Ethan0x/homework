ca50addca91bac45767330abcfdcf1c83925f179
Kook <jj.mac@live.cn>
2021-08-29 15:14:57 +0800

    Add 31.next_permutation

---
+++ week1/31.next_permutation-下一个排列/31.next_permutation.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function (nums) {
+    let i = nums.length - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+        i--;
+    }
+
+    if (i >= 0) {
+        let j = nums.length - 1;
+        while (j >= 0 && nums[i] >= nums[j]) {
+            j--;
+        }
+        swap(nums, i, j);
+    }
+    reverse(nums, i + 1);
+};
+
+var swap = function (nums, i, j) {
+    let tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
+};
+
+var reverse = function (nums, start) {
+    let left = start, right = nums.length - 1;
+    while (left < right) {
+        swap(nums, left, right);
+        left++;
+        right--;
+    }
+}

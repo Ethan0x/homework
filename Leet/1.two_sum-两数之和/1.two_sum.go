9229875c8c926b1f42ef8448fe3fbb6d036cbacb
Kook0x <AppleGenius_Liu@me.com>
2023-03-10 12:11:23 +0800

    Add Array useage & refactor path

--- Leet/1.two_sum-两数之和/1.two_sum.go
+++ Leet/1.two_sum-两数之和/1.two_sum.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // O(n^2)
 func twoSum(nums []int, target int) []int {
 	var result = make([]int, 2)
@@ -13,8 +15,21 @@ func twoSum(nums []int, target int) []int {
 	return result
 }
 
+//func twoSum2(nums []int, target int) []int {
+//	hm := map[int]int{}
+//	for i := 0; i < len(nums); i++ {
+//		v := nums[i]
+//		if idx, ok := hm[target-v]; ok {
+//			return []int{idx, i}
+//		}
+//		hm[v] = i
+//	}
+//	return nil
+//}
+
 func twoSum2(nums []int, target int) []int {
 	hm := map[int]int{}
+
 	for i := 0; i < len(nums); i++ {
 		v := nums[i]
 		if idx, ok := hm[target-v]; ok {
@@ -23,4 +38,17 @@ func twoSum2(nums []int, target int) []int {
 		hm[v] = i
 	}
 	return nil
+}
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+
+	result := twoSum2(nums, target)
+	if len(result) > 0 {
+		fmt.Printf("nums[%d] = %d\n", result[1], result[0])
+	} else {
+		fmt.Println("Can't found target in array nums")
+	}
+
 }
